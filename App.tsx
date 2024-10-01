188e059cd35cc457bfd55956f42ce3229cb0a8ed
Hai Le <evanle2004@gmail.com>
2024-10-01 11:26:20 -0700

    hello

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Josiah was here</Text>
+      <Text>Hai was here</Text>
       <StatusBar style="auto" />
     </View>
   );

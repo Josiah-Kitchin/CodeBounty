046d782e01ab65292c50986575b57aaaf49957c4
miro <mirop@uoregon.edu>
2024-10-01 11:28:02 -0700

    hello, hi

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Josiah was here</Text>
+      <Text>Miro was here</Text>
       <StatusBar style="auto" />
     </View>
   );

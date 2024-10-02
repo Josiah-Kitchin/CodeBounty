5caa8a1f862f95364496d19d680e5f50f4424b61
Josiah <jkitchi3@uoregon.edu>
2024-10-02 09:21:20 -0700

    changed a test

--- __tests__/test_components/test_headers.js
+++ __tests__/test_components/test_headers.js
@@ -4,6 +4,8 @@ import { render } from '@testing-library/react-native';
 import MainHeader from '../../src/components/headers/main_header'; 
 
 test('renders correctly', () => {
-  const { getByText } = render(<MainHeader mainText='Hello World'/>);
+  const { getByText } = render(<MainHeader mainText='Hello World' subText="hi there"/>);
   expect(getByText('Hello World')).toBeTruthy();
+  expect(getByText('hi there')).toBeTruthy(); 
 });
+

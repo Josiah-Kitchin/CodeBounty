654e451955ac516b46ad254eaa7b6b31ed2a35f5
Josiah <jkitchi3@uoregon.edu>
2024-10-04 22:50:41 -0700

    revererted database, thats a problem for another day

--- App.tsx
+++ App.tsx
@@ -9,25 +9,6 @@ import React, {useState} from 'react';
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import IntroScreen from './src/screens/intro';
-import firebase from '@react-native-firebase/app';
-import firestore from '@react-native-firebase/firestore';
-
-const db = firestore();
-const usersCollection = db.collection('users');
-usersCollection.add({
-  name: 'John Doe',
-  age: 30,
-  location: 'New York',
-  // ... other user data
-})
-.then(docRef => {
-  console.log('Document written with ID:', docRef.id);
-})
-.catch(error => {
-  console.error('Error adding document:', error);
-});
-
-
 
 
 const App = () => {

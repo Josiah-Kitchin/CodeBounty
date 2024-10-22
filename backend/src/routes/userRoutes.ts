81682af62aaaad3e857e664b4e5aa55f60c464b2
Josiah <jkitchi3@uoregon.edu>
2024-10-21 20:13:26 -0700

    seperated route logic into the controller

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,33 +1,14 @@
 
 import { Router, Request, Response } from 'express';
-import UserModel from '../models/userModel.js'
+import { createUser, getUserNameById } from '../controllers/userController.js';
 
 const router = Router(); 
-const userModel = new UserModel();
-
 
 //Create a new user request 
-router.post('/api/users', async (req: Request, res: Response): Response => {
-    try {
-	await userModel.create(req.body);
-	return res.status(201).json({ message: "User Created"} );
-    } catch (error) { 
-	return res.status(400).json( {error: error.message} );
-    }
-});
+router.post('/api/users', createUser);   
 
 //Get a user's name by id
-router.get('/api/users/:id/name', async (req: Request, res: Response): Response => {
-    try {
-	const id = req.params.id; // Get the ID from the route parameters
-	const userName = await userModel.getNameById(id);
-	return res.status(200).json({ name: userName });
-    } catch (error) {
-	if (error.message == "User Not Found") {
-	    return res.status(404).json( {error: error.message });
-	} 
-	return res.status(500).json( {error: error.message });
-    }
-});
+router.get('/api/users/:id/name', getUserNameById); 
 
 export default router; 
+
